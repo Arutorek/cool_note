ac86eae79246605418d30d6024f61215acc001f8
Jakub Strzelecki <arutorek@gmail.com>
2022-05-12 14:40:23 +0200

    test

---
+++ src/main/java/com/codecool/java/CoolNoteJavaApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolNoteJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolNoteJavaApplication.class, args);
+	}
+
+}
